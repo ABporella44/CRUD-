b10854b5d84e5b3d351b0458b918dc0f8e839723
Porella abhilash <porella.mallikanti@gmail.com>
2021-08-04 10:02:19 +0530

    This is the first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import Axios from 'axios';
+
+
+
+
+
 
 function App() {
+
+  const [movieName, setMovieName] = useState(" ");
+  const [movieReview, setMovieReview] = useState(" ");
+  const enterMovieName =(e) =>{setMovieName(e.target.value)}
+  const enterMoviereview =(e) =>{setMovieReview(e.target.value)}
+
+  const submit = (e) => {
+    Axios.post("http://localhost:3001/insert/api",{ID:2,movieName:movieName,movieReview:movieReview}).then(()=>{
+      alert("Successfull Insert");
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <h1>Crud Application</h1>
+     <div className="form">
+      <label>MovieName:</label>
+       <input onChange={enterMovieName} type="text" name="movieName"/> <br/>
+       <label>Review:</label>
+       <input onChange={enterMoviereview} type="text" name="review"/>  <br/>
+       <button onClick={submit}>Submit</button>
+     </div>
+     </div>
   );
 }
 
